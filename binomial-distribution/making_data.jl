c28ff7203cd17df4900aa84d8efe1c404bde25b6
fanyCaz <stepheniecaz@gmail.com>
2021-06-13 22:32:48 -0500

    several distributions

--- binomial-distribution/making_data.jl
+++ binomial-distribution/making_data.jl
@@ -23,11 +23,10 @@ end
 
 # Get the experiences of diffentes scenarios
 positives_per_sample = Int64[]
-for i = 1:50
+for i = 1:10000
   push!(positives_per_sample, customerRatings() )
 end
 
-println(positives_per_sample)
 println("How many of these samples achieve the 100% rating?")
 number_perfect_rating = count(i-> i == 10, positives_per_sample)
 number_samples = length(positives_per_sample)
@@ -40,35 +39,25 @@ println("$number_perfect_rating out of $number_samples have perfect score, this 
 # The 'problem' with the success rate, is that it keeps changing over time.
 # For now, let's keep the problem with success rate of 0.95
 s = 0.95
-println(positives_per_sample)
 rating_scale = 1:10
 ratings_distribution = Int64[]
 
+println("Distribution")
 for i ∈ rating_scale
-  print("number:  $i ")
-  push!(ratings_distribution, count(x-> x == i, positives_per_sample))
+  quantity = count(x-> x == i, positives_per_sample)
+  println("Quantity of $i : $quantity")
 end
 
-println("Distribucion")
-println("$ratings_distribution . number samples $number_samples")
-quantity_scale = convert(Vector{Int},1:10)
-quantity_scale = 1:10
-K = Int(round(1 + 3.322 * log(length(positives_per_sample))))
-# fit data in histogram
-h = fit(Histogram, ratings_distribution, nbins= 10)
-# h.weights frequency
-println("Edges $(h.edges) Weights $(h.weights)")
-
+# histogram( population, bins how is divided)
 p_histogram = histogram(
-                  positives_per_sample,
-                  bins = :scott,
-                  xlabel = "Probability",
-                  ylabel = "Frequency",
-                  title = "Probabilities of ratings distribution",
-                  legend = false,
-                  color = :dodgerblue
-               )
-savefig("Distribution.png")
-#=
-p_scatter = scatter(t,observations, title="scattered")
-=#
+                positives_per_sample,
+                bins = :scott,
+                xlabel = "Probability",
+                ylabel = "Frequency",
+                title = "Probabilities of ratings distribution",
+                legend = false,
+                color = :dodgerblue
+              )
+
+savefig("Distribution_$number_samples.png")
+
