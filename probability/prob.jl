8276a84049cca4508327f67416c3d0bb5bef11eb
fanyCaz <stepheniecaz@gmail.com>
2021-12-19 19:18:41 -0600

    dice probability

--- probability/prob.jl
+++ probability/prob.jl
@@ -1,7 +1,6 @@
 function throw_dice(throws_number)
   throw_sequence = Int64[]
   for i ∈ 1:throws_number
-    println(i)
     throw = rand(1:6)
     push!(throw_sequence, throw)
   end
@@ -9,25 +8,26 @@ function throw_dice(throws_number)
 end
 
 function main()
-  throws_number = 1
-  attempts_number = 100
-  # The more population tests, the more accurate the simulation is
+  # Obtener un 12 con dos dados, y 10 tiros
+  throws_number = 10
+  attempts_number = 10000
   throws = []
+  one_counter = 0
+  # The more population tests, the more accurate the simulation is
   for i ∈ 1:attempts_number
     throw_sequence = throw_dice(throws_number)
+    second_throw_sequence = throw_dice(throws_number)
+    append!(throw_sequence, second_throw_sequence)
     push!(throws, throw_sequence)
+    # Counts if a 1 is found in the sequence
+    #one_counter += any(x -> x == 1, throw_sequence) ? 1 : 0
+    if 1 ∉ throw_sequence
+      one_counter += 1
+    end
   end
-  println("Throws: ")
-  println(throws)
-  println("Throws with a 1")
-  throws_with_one = 0
-  for sequence ∈ throws
-    throws_with_one += count(x -> x == 1, sequence)
-  end
-  println(throws_with_one)
-  println("Probability a 1 is in a throw")
-  probability = throws_with_one/attempts_number
-  println(probability)
+  println("Number of one's found: $one_counter - Number of attempts: $attempts_number")
+  probability = one_counter/attempts_number
+  println("Probability of not getting a one: $probability")
 end
 
 main()
