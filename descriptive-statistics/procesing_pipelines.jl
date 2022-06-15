386936a9ed0ab07501fb352cbb6190af90d04a0b
fanyCaz <stepheniecaz@gmail.com>
2022-06-14 22:51:18 -0500

    different methods to scale

--- descriptive-statistics/procesing_pipelines.jl
+++ descriptive-statistics/procesing_pipelines.jl
@@ -1,5 +1,7 @@
 using CSV
 using DataFrames
+using Statistics
+using Plots
 
 function min_max(data,column)
   max_value = maximum(data[:,column])
@@ -8,10 +10,38 @@ function min_max(data,column)
   return norm
 end
 
+function z_score(data,column)
+  mean_x = mean(data[:,column])
+  std_x  = std(data[:,column])
+  return (data[:,column] .- mean_x) ./ std_x
+end
+
+function print_distribution(values)
+  p_histogram = histogram(
+                          values,
+                          bins = :scott,
+                          legend= false,
+                          color = :dodgerblue
+                         )
+  savefig("Distribucion_sqrt.png")
+end
+
+function hip_tan(data,column,setting)
+  return map(x-> tanh(x/setting),data[:,column])
+end
+
+function square_root_data(data,column)
+  return map(x-> √(x), data[:,column])
+end
+
 function main()
+  gr(size=(800,450))
   cars_df = CSV.read("vehicle_s.csv", DataFrame)
   norm = min_max(cars_df,"compactness")
-  print(norm)
+  norm = z_score(cars_df,"compactness")
+  norm = hip_tan(cars_df,"compactness",1000)
+  norm = square_root_data(cars_df,"compactness")
+  print_distribution(norm)
 end
 
 main()
